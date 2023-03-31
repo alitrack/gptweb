effee2cb9b4386b28f2851b0984a07513d11641f
Steven Lee <admin@alitrack.com>
2023-03-31 14:28:25 +0000

    vercel support

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+// Set up a proxy to forward requests to the OpenAI API endpoint
+
+// To use the OpenAI API, you will need an API key that can be set as an environment variable (OPENAI_API_KEY) 
+//or integrated into the front-end client.  
+module.exports = async function  openAIProxy(req, res, next) {
+  let target = '';
+  let openApiKey = process.env.OPENAI_API_KEY || '';
+  const options = {
+      target:  "https://api.openai.com",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "" // strip "/api" from the URL
+      },
+      onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+      },
+    };
+    if(openApiKey !== ''){
+      options.headers= { 
+        Authorization: `Bearer ${openApiKey}` 
+      };
+    }
+    return createProxyMiddleware(options)(req, res, next);
+  }
+
+// export const config = {
+//     api: {
+//         bodyParser: false, // enable POST requests
+//         externalResolver: true, // hide warning message
+//     },
+// };
--- app.js
+++ app.js
@@ -2,34 +2,36 @@
 const express = require('express'); // Express framework
 const path = require('path'); // Node.js path module
 const bodyParser = require('body-parser'); // Body parsing middleware
-const { createProxyMiddleware } = require('http-proxy-middleware'); // HTTP proxy middleware
+// const  {openAIProxy} = require('api/index.mjs');
+const openAIProxy = require('./api/index')
 
 // Create an instance of the Express application
 const app = express();
 
 // Set the port number for the application to listen on
 const port = process.env.PORT || 9000;
-const openApiKey = process.env.OPENAI_API_KEY || '';
-// Set up a proxy to forward requests to the OpenAI API endpoint
-app.use('/v1/chat/completions', (req, res, next) => {
-  const options = {
-    target: process.env.TARGET || "https://api.openai.com",
-    changeOrigin: true,
-    onProxyRes: function (proxyRes, req, res) {
-      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    },
-  };
+app.use('/v1/chat/completions', openAIProxy);
+// const openApiKey = process.env.OPENAI_API_KEY || '';
+// // Set up a proxy to forward requests to the OpenAI API endpoint
+// app.use('/v1/chat/completions', (req, res, next) => {
+//   const options = {
+//     target: process.env.TARGET || "https://api.openai.com",
+//     changeOrigin: true,
+//     onProxyRes: function (proxyRes, req, res) {
+//       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//     },
+//   };
 
-  // To use the OpenAI API, you will need an API key that can be set as an environment variable (OPENAI_API_KEY) 
-  //or integrated into the front-end client.
+//   // To use the OpenAI API, you will need an API key that can be set as an environment variable (OPENAI_API_KEY) 
+//   //or integrated into the front-end client.
 
-  if(openApiKey != ''){
-    options.headers= { 
-      Authorization: `Bearer ${openApiKey}` 
-    };
-  }
-  createProxyMiddleware(options)(req, res, next);
-});
+//   if(openApiKey != ''){
+//     options.headers= { 
+//       Authorization: `Bearer ${openApiKey}` 
+//     };
+//   }
+//   createProxyMiddleware(options)(req, res, next);
+// });
 
 // Use body-parser middleware to parse request bodies
 app.use(bodyParser.json());
